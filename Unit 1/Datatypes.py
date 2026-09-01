3f0761ebc7f3a1f43241b8a79143614cdac1ef35
MNeal8 <MNeal814@boyslatin.net>
2026-09-01 12:51:08 +0000

    Data types  notes/examples

---
+++ Unit 1/Datatypes.py
@@ -0,0 +1,13 @@
+#String: is a block of characters wrapped in quotes. Characters can be numbers, letters, and symbols
+"5417 Larchwood Ave"
+"Mekhi"
+the hashtag is a marker that makes anything written
+# behind it invisiable to the computer
+Integers: Data types that are whole numbers or numbers without decimal point.
+example: 2333333333354
+Dont need to add any symbols.
+ Floats: Just like integers except they're decimal numbers.
+ Example: 3.14
+ Booleans: can either be true or false
+ No using quotations marks for boolean values. The computer understands the words that are unique.
+ 
