d97184a915865936c7bdfd13712cbe2cfaad5fe6
MNeal8 <MNeal814@boyslatin.net>
2026-09-16 12:56:26 +0000

    Part 2 of True or False Scenerios

---
+++ Unit 1/operaters.py
@@ -0,0 +1,32 @@
+# Operater Families:
+
+# Assignment Operaters: Used to assign values to variables. Key / value pairings
+# we use the equal sign to represent the assignment operater
+Example: Name = "Mekhi"
+
+# Arithmetic Operaters: Used on numerical data types to perform calculations.
+# print is a function that lets us show code in the terminal
+
+print( 3 + 3 ) # Addition operater
+print ( 3 - 3 ) # Subtraction operater
+print( 4 * 3 ) # Multiplaction operater
+print( 12 / 3 ) # division operater
+
+# Comparison Operaters: Set of symbols useed to assess if data is the same or different and how they're different.
+
+print(10 > 1) # greater than operater
+print( 10 < 2 ) # less than operater  
+# 2 equal signs compare if something is the SAME 
+print(2.0 == 2) # same as (true)
+
+# not equal is written !=
+# this is to check and filter for values that are not the same
+print(200 != 100) # true- these are not the same 
+print(392 != 179)
+
+# Logical Operater:
+# compares 2 conditions to check if they're true or false
+# and checks if 2 conditions are true. If yes, the final result is true.
+print(3 > 1 and 100 > 50) 
+# OR: Checks if 1 condition is true. If yes, the final result is true
+print(3 > 1 or 100 > 50) 
---
+++ Unit 1/operatersActivity.py
@@ -0,0 +1,27 @@
+# 1 false 
+print(200 == 100)
+
+#2 False
+print(15 > 18)
+
+#3 False
+print("Coding 1" == "coding1")
+
+#4 False
+print("0" == 0)
+
+#5 410.98
+print( 10.99 + 399.99)
+
+#6 True
+Recommendation = True
+print(86 > 85 and Recommendation == True )
+
+#7 True
+parent1 = False
+parent2 = True
+print(parent1 == False or parent2 == True)
+
+#8 False
+print(98.87 >= 100) 
+       
