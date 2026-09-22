d74b4c39e5622a02c542670b6850240d48f16f5c
MNeal8 <MNeal814@boyslatin.net>
2026-09-22 12:52:57 +0000

    class notes

---
+++ Unit 2/builtinFunctions.py
@@ -0,0 +1,28 @@
+# function - simply put a code block of instructions for computers to follow
+
+# Built in Function - a code block of instruction for a computer that was already written for us
+
+# input() a built in function that allows a user to type in data
+#print() a built in function that allows a user to print data in the terminal 
+#name = input("what is your name?")
+
+print('name')
+
+# Data casting functiond
+# these are built in (pre-written) functions
+#that change data types from one form into another
+
+#str () this datacasting function allows you to change any data type into a string
+# String concatenation - combining string data types with one another. We use the (+) sign operater to combine strings together
+
+
+year = 1906
+str()
+
+print('This event took place in ' + str(year))
+
+# int()  - any data type passed into the brackets will be converted into an integer(whole number)
+price = 134.98 
+print(int(price)) 
+# transforms a decimal into a whole number
+#  cannot transform strings into numbers
